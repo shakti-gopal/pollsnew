78d70287cc52d33a38def3a2706c865d2b76b0ea
shakti-gopal <30345956+shakti-gopal@users.noreply.github.com>
2017-08-10 15:15:19 +0530

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib import admin
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    url(r'^polls/', views.IndexView.as_view(), name='index'),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
