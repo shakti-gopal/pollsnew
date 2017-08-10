d0b5626747a35ebbd59ab35ca461e1a6170c653c
shakti-gopal <30345956+shakti-gopal@users.noreply.github.com>
2017-08-10 15:17:12 +0530

    Update settings.py

--- polls/settings.py
+++ polls/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
